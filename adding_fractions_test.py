ccc565dcefc5525c627b5fd7209a2927f7f95293
Hemal Varambhia <h.n.varambhia@gmail.com>
2019-08-16 11:34:39 +0100

    Got a failing hookup test passing.

---
+++ adding_fractions_test.py
@@ -0,0 +1,8 @@
+import unittest
+class AddingFractionTest(unittest.TestCase):
+    def testHookUp(self):
+        assert 0 + 0 == 0
+
+if __name__ == "__main__":
+
+    unittest.main() # run all tests

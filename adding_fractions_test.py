ff7cd83a2f297d747b994ff821bd84634ff88de3
Hemal Varambhia <h.n.varambhia@gmail.com>
2019-08-16 11:44:38 +0100

    Got 0 + 0 test case working with fake it 'til you make it.

--- adding_fractions_test.py
+++ adding_fractions_test.py
@@ -1,7 +1,20 @@
 import unittest
+class Fraction:
+   def __init__(self, numerator):
+       pass
+
+   def plus(self, augend):
+       return 0
+
 class AddingFractionTest(unittest.TestCase):
     def testHookUp(self):
         assert 0 + 0 == 0
+
+    def test_that_zero_plus_zero_equals_zero(self):
+        addend = Fraction(0)
+        augend = Fraction(0)
+        sum = addend.plus(augend)
+        assert sum == 0, 'Expected 0, got ' + str(sum)
 
 if __name__ == "__main__":
 

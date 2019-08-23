8b92f0a149e33fc5be00fdef6a107e86c24e8fc7
Hemal Varambhia <h.n.varambhia@gmail.com>
2019-08-23 13:35:35 +0100

    Removed duplicate code from the tests.

--- adding_fractions_test.py
+++ adding_fractions_test.py
@@ -16,46 +16,43 @@ class AddingFractionsTest(unittest.TestCase):
         addend = Fraction(0)
         augend = Fraction(0)
         sum = addend + augend
-        expected = 0
-        self.assert_equals(sum, expected)
+        self.assert_equals(sum, 0)
 
     def test_that_zero_plus_one_equals_one(self):
         addend = Fraction(0)
         augend = Fraction(1)
         sum = addend + augend
-        expected = 1
-        self.assert_equals(sum, expected)
+        self.assert_equals(sum, 1)
 
     def test_that_one_plus_one_equals_two(self):
         addend = Fraction(1)
         augend = Fraction(1)
         sum = addend + augend
-        expected = 2
-        self.assert_equals(sum, expected)
+        self.assert_equals(sum, 2)
 
     def test_that_minus_one_plus_zero_equals_minus_one(self):
         addend = Fraction(-1)
         augend = Fraction(0)
         sum = addend + augend
-        assert sum == -1, ' Expected -1, got ' + str(sum)
+        self.assert_equals(sum, -1)
 
     def test_that_minus_one_plus_one_equals_zero(self):
         addend = Fraction(-1)
         augend = Fraction(1)
         sum = addend + augend
-        assert sum == 0, ' Expected 0, got ' + str(sum)
+        self.assert_equals(sum, 0)
 
     def test_that_minus_one_plus_two_equals_one(self):
         addend = Fraction(-1)
         augend = Fraction(2)
         sum = addend + augend
-        assert sum == 1, ' Expected 1, got ' + str(sum)
+        self.assert_equals(sum, 1)
 
     def test_that_minus_one_plus_minus_one_equals_minus_two(self):
         addend = Fraction(-1)
         augend = Fraction(-1)
         sum = addend + augend
-        assert sum == -2, ' Expected -2, got ' + str(sum)
+        self.assert_equals(sum, -2)
 
     def assert_equals(self, sum, expected):
         assert sum == expected, 'Expected ' + str(expected) + ', got ' + str(sum)

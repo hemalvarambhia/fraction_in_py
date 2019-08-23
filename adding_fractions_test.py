44a1f4eb588f28e8a5955121dec1bce5a4d1a3c4
CMcDonagh01 <53273390+CMcDonagh01@users.noreply.github.com>
2019-08-23 14:02:19 +0100

    Update adding_fractions_test.py
    Can now add fractions where the denominators remain the same after addition

--- adding_fractions_test.py
+++ adding_fractions_test.py
@@ -1,11 +1,14 @@
 import unittest
 class Fraction:
-   def __init__(self, numerator):
+   def __init__(self, numerator, denominator = 1):
        self.numerator = int(numerator)
-       pass
-
+       self.denominator = int(denominator)
+       
    def __add__(self, augend):
-      return int(self.numerator) + int(augend.numerator)
+      return Fraction(int(self.numerator) + int(augend.numerator), 3)
+
+   def as_integer(self):
+      return self.numerator
 
 class AddingFractionsTest(unittest.TestCase):
     def testHookUp(self):
@@ -16,43 +19,51 @@ class AddingFractionsTest(unittest.TestCase):
         addend = Fraction(0)
         augend = Fraction(0)
         sum = addend + augend
-        self.assert_equals(sum, 0)
+        self.assert_equals(sum.as_integer(), 0)
 
     def test_that_zero_plus_one_equals_one(self):
         addend = Fraction(0)
         augend = Fraction(1)
         sum = addend + augend
-        self.assert_equals(sum, 1)
+        self.assert_equals(sum.as_integer(), 1)
 
     def test_that_one_plus_one_equals_two(self):
         addend = Fraction(1)
         augend = Fraction(1)
         sum = addend + augend
-        self.assert_equals(sum, 2)
+        self.assert_equals(sum.as_integer(), 2)
 
     def test_that_minus_one_plus_zero_equals_minus_one(self):
         addend = Fraction(-1)
         augend = Fraction(0)
         sum = addend + augend
-        self.assert_equals(sum, -1)
+        self.assert_equals(sum.as_integer(), -1)
 
     def test_that_minus_one_plus_one_equals_zero(self):
         addend = Fraction(-1)
         augend = Fraction(1)
         sum = addend + augend
-        self.assert_equals(sum, 0)
+        self.assert_equals(sum.as_integer(), 0)
 
     def test_that_minus_one_plus_two_equals_one(self):
         addend = Fraction(-1)
         augend = Fraction(2)
         sum = addend + augend
-        self.assert_equals(sum, 1)
+        self.assert_equals(sum.as_integer(), 1)
 
     def test_that_minus_one_plus_minus_one_equals_minus_two(self):
         addend = Fraction(-1)
         augend = Fraction(-1)
         sum = addend + augend
-        self.assert_equals(sum, -2)
+        self.assert_equals(sum.as_integer(), -2)
+
+    def test_that_2_on_5_plus_1_on_5_equals_3_on_5(self):
+       addend = Fraction(1,3)
+       augend = Fraction(1,3)
+       sum = addend + augend
+       self.assert_equals(sum.numerator, 2)
+       self.assert_equals(sum.denominator, 3)
+       
 
     def assert_equals(self, sum, expected):
         assert sum == expected, 'Expected ' + str(expected) + ', got ' + str(sum)

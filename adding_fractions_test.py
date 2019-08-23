b141680f7ca07025b0ecdfb4664018074debd561
CMcDonagh01 <53273390+CMcDonagh01@users.noreply.github.com>
2019-08-23 12:57:06 +0100

    Update adding_fractions_test.py
    Can now add any two integers

--- adding_fractions_test.py
+++ adding_fractions_test.py
@@ -1,20 +1,34 @@
 import unittest
 class Fraction:
    def __init__(self, numerator):
+       self.numerator = int(numerator)
        pass
-
+       
    def plus(self, augend):
-       return 0
+       return int(self.numerator) + int(augend.numerator)
 
 class AddingFractionsTest(unittest.TestCase):
     def testHookUp(self):
         assert 0 + 0 == 0
+        assert 0 + 1 == 1
 
     def test_that_zero_plus_zero_equals_zero(self):
         addend = Fraction(0)
         augend = Fraction(0)
         sum = addend.plus(augend)
         assert sum == 0, 'Expected 0, got ' + str(sum)
+
+    def test_that_zero_plus_one_equals_one(self):
+        addend = Fraction(0)
+        augend = Fraction(1)
+        sum = addend.plus(augend)
+        assert sum == 1, 'Expected 1, got ' + str(sum)
+
+    def test_that_one_plus_one_equals_two(self):
+        addend = Fraction(1)
+        augend = Fraction(1)
+        sum = addend.plus(augend)
+        assert sum == 2, 'Expected 1, got ' + str(sum)
 
 if __name__ == "__main__":
 

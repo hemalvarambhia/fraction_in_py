c00512cc3ee2d81475c0d800e30e9e1c3cf0bee4
hemalvarambhia <h.n.varambhia@gmail.com>
2023-05-16 23:15:01 +0100

    We can simplify a fraction whose denominator is negative.

--- src/fraction.py
+++ src/fraction.py
@@ -2,8 +2,9 @@ import math
 class Fraction:
     def __init__(self, numerator, denominator=1):
         divisor = math.gcd(int(numerator), int(denominator))
-        self.numerator = int(numerator / divisor)
-        self.denominator = int(denominator / divisor)
+        multiplier = -1 if denominator < 0 else 1
+        self.numerator = multiplier * int(numerator / divisor)
+        self.denominator = multiplier * int(denominator / divisor)
 
     def __add__(self, augend):
         lcm = math.lcm(augend.denominator, self.denominator)  # lowest common multiple
--- tests/simplifying_fractions_test.py
+++ tests/simplifying_fractions_test.py
@@ -13,6 +13,9 @@ class SimplifyingFractions(unittest.TestCase):
     def test_simplifying_a_fraction_with_a_negative_numerator(self):
         self.assert_equals(Fraction(-5, 10), Fraction(-1, 2))
 
+    def test_simplifying_a_fraction_with_a_negative_numerator(self):
+        self.assert_equals(Fraction(-2, -6), Fraction(1, 3))
+
     def assert_equals(self, sum, expected):
         assert sum.numerator == expected.numerator, 'Numerator: expected: ' + str(expected) + ', but got ' + str(sum.numerator)
         assert sum.denominator == expected.denominator, 'Denominator: expected: ' + str(expected) + ', but got ' + str(sum.denominator)

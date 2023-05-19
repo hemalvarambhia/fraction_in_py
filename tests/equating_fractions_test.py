69030aa20f2f32fb47c386766ef648aadfe4704e
hemalvarambhia <h.n.varambhia@gmail.com>
2023-05-19 21:26:35 +0100

    Used the framework's assertion method.

--- tests/equating_fractions_test.py
+++ tests/equating_fractions_test.py
@@ -18,7 +18,7 @@ class MyTestCase(unittest.TestCase):
         f = Fraction(4, 5)
         g = Fraction(3, 5)
 
-        assert f != g
+        self.assertNotEqual(f, g)
 
 
 if __name__ == '__main__':

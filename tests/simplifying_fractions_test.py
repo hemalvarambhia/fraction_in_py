6dd1787085fe7db29c010116c041f47127036708
hemalvarambhia <h.n.varambhia@gmail.com>
2023-05-21 09:54:02 +0100

    Inlined the custom assertion.

--- tests/simplifying_fractions_test.py
+++ tests/simplifying_fractions_test.py
@@ -3,19 +3,25 @@ from fraction import Fraction
 
 class SimplifyingFractions(unittest.TestCase):
     def test_fraction_that_is_already_simplified(self):
-        self.assert_equals(Fraction(3, 4), Fraction(3, 4))  # add assertion here
+        sum1 = Fraction(3, 4)
+        expected = Fraction(3, 4)
+        self.assertEqual(sum1, expected)
 
     def test_simplifying_a_fraction_with_a_common_divisor(self):
-        self.assert_equals(Fraction(5, 10), Fraction(1, 2))
+        sum1 = Fraction(5, 10)
+        expected = Fraction(1, 2)
+        self.assertEqual(sum1, expected)
 
     def test_simplifying_a_fraction_with_a_negative_numerator(self):
-        self.assert_equals(Fraction(-5, 10), Fraction(-1, 2))
+        sum1 = Fraction(-5, 10)
+        expected = Fraction(-1, 2)
+        self.assertEqual(sum1, expected)
 
     def test_simplifying_a_fraction_with_a_negative_numerator(self):
-        self.assert_equals(Fraction(-2, -6), Fraction(1, 3))
+        sum1 = Fraction(-2, -6)
+        expected = Fraction(1, 3)
+        self.assertEqual(sum1, expected)
 
-    def assert_equals(self, sum, expected):
-        self.assertEqual(sum, expected)
 
 if __name__ == '__main__':
     unittest.main()
